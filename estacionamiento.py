5dc1c4e5cd49742491110b4b20ad584b163e397a
Alan Albertini <alanjack78@gmail.com>
2026-07-06 23:10:03 -0300

    limpieza de pantalla, funcion de ENTER y control de errores.

--- estacionamiento.py
+++ estacionamiento.py
@@ -1,3 +1,11 @@
+import os
+
+def apretar_enter():
+    input("\nAPRETE ENTER PARA CONTINUAR...")
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def calcular_cobro(horas):
     if horas < 1:
         return 2000
@@ -9,6 +17,7 @@ autos_atendidos = 0
 recaudacion_total = 0
 
 while True:
+    limpiar_pantalla()
     print("\n--- SISTEMA DE ESTACIONAMIENTO ---")
     print("\n1. Ingresar vehículo")
     print("2. Retirar vehículo")
@@ -17,22 +26,38 @@ while True:
     opcion = input("\nElegí una opción (1-3): ")
     
     if opcion == "1":
+        limpiar_pantalla()
         if espacios_disponibles > 0:
             espacios_disponibles = espacios_disponibles - 1
             autos_atendidos = autos_atendidos + 1
             print("VEHÍCULO INGRESADO. QUEDAN", espacios_disponibles, "ESPACIOS DISPONIBLES")
         else:
             print("ESTACIONAMIENTO LLENO! NO QUEDAN LUGARES DISPONIBLES")
+        apretar_enter()  
+
     elif opcion == "2":
+        limpiar_pantalla()
         print("\nSE VA A RETIRAR UN VEHÍCULO")
-        espacios_disponibles = espacios_disponibles + 1
-        hora_estacionado = float(input("\nCUANTAS HORAS ESTUVO ESTACIONADO? ")) 
-        costo_estacionado = calcular_cobro(hora_estacionado)     
-        recaudacion_total = recaudacion_total + costo_estacionado   
+        if espacios_disponibles == 10:
+            print("\nERROR! No hay autos ingresados, el estacionamiento está vacío!")
+            apretar_enter()
+        else:
+            try:
+                espacios_disponibles = espacios_disponibles + 1
+                hora_estacionado = float(input("\nCUANTAS HORAS ESTUVO ESTACIONADO?: ")) 
+                costo_estacionado = calcular_cobro(hora_estacionado)     
+                recaudacion_total = recaudacion_total + costo_estacionado  
+            except ValueError:
+                print("\nERROR! ingrese un número válido...")
+                apretar_enter()    
+
     elif opcion == "3":
+        limpiar_pantalla()
         print("\nPROGRAMA FINALIZADO")
         print("\nAUTOS ATENDIDOS: ", autos_atendidos)
-        print("RECAUDACION TOTAL: ", recaudacion_total)
+        print("RECAUDACION TOTAL: $", recaudacion_total)
+        apretar_enter()
         break   
     else:
         print("\nERROR!, opción inválida")    
+        apretar_enter
